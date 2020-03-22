5118ef264bdafa80b9da02f63bdcf50af257f5d8
tiansai <44078316@qq.com>
2020-03-23 01:43:13 +0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// console.log("hello".length);
+
+// console.log("hello".charAt(0));
+
+// console.log("hello, world".replace("hello","hi"));
+
+// console.log("hello, world".toUpperCase());
+
+
+// var name = "zhangsan";
+
+// console.log(name);
+
+// name = "lisi";
+
+// console.log(name);
+
+// const pi = 3.14;
+
+// console.log(pi);
+
+
+// var x;
+// x = "3"+4;
+// console.log(x);
+
+// console.log(123 == "123");
+
+// console.log(123 === "123");
+
+// ==有类型转换
+// ===没有类型转换
+
+  
+
